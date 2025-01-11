f4c6f4c10c634f7c200650d37607f812d5d8b870
Rajesh-107 <applerb326@gmail.com>
2025-01-11 11:46:54 +0600

    updated with style

--- src/app/Specialist/Specialist.tsx
+++ src/app/Specialist/Specialist.tsx
@@ -1,7 +1,16 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 
 
-const Specialist = () => {
+const Specialist = async () => {
+    const res = await fetch("http://localhost:5100/api/v1/specialties", {
+        next: {
+            revalidate: 5
+        }
+    })
+    const { data: spe } = await res.json()
+
+
     return (
         <Container>
             <Box sx={{
@@ -15,8 +24,36 @@ const Specialist = () => {
                     <Typography variant="h4" fontWeight={600}>Explore Treatment Across Specialties</Typography>
                     <Typography component="p" fontSize={18} fontWeight={300}>Lorem ipsum dolor sit, amet consectetur adipisicing.</Typography>
                 </Box>
+                <Stack direction="row" gap={4} mt={5}>
+                    {spe.map((specility: any) => (
+                        <Box key={specility.id} sx={{
+                            flex: 1,
+                            width: "150px",
+                            backgroundColor: "rgba(245,245,245,1)",
+                            border: "1px solid rgba(255,255,255, 1)",
+                            borderRadius: "10px",
+                            textAlign: "center",
+                            padding: "40px 10px",
+                            "& img": {
+                                width: '50px',
+                                height: '50px',
+                                margin: "0 auto",
+                            },
+                            "&:hover": {
+                                border: "1px solid blue",
+                            },
+                        }}
+                        >
+                            <Image src={specility.icon} height={100} width={100} alt="specility" />
+
+                            <Box>
+                                <Typography component="p" fontSize={18} fontWeight={300} mt={2}>{specility.title} </Typography>
+                            </Box>
+                        </Box>
+                    ))}
+                </Stack>
             </Box>
-        </Container>
+        </Container >
     );
 };
 
