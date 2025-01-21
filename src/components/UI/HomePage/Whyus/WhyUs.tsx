18d7070e8214c472e9a22c7d80ea1fe8b7673dc6
Rajesh-107 <applerb326@gmail.com>
2025-01-22 00:09:46 +0600

    why us

---
+++ src/components/UI/HomePage/Whyus/WhyUs.tsx
@@ -0,0 +1,16 @@
+import { Box, Container, Typography } from "@mui/material";
+
+
+const WhyUs = () => {
+    return (
+        <Container>
+            <Box>
+                <Typography>
+                    Why Us
+                </Typography>
+            </Box>
+        </Container>
+    );
+};
+
+export default WhyUs;
