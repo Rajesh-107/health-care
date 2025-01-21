c1d9fc83f7f1e978b840e4b37135840cfdc72a81
Rajesh-107 <applerb326@gmail.com>
2025-01-21 22:18:52 +0600

    updated with doctor

--- src/components/UI/HomePage/TopRatedDoctor/TopRatedDoctors.tsx
+++ src/components/UI/HomePage/TopRatedDoctor/TopRatedDoctors.tsx
@@ -1,72 +1,111 @@
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
+import Link from "next/link";
 
 const TopRatedDoctors = async () => {
     const res = await fetch("http://localhost:5100/api/v1/doctor?page=1&limit=3")
     const { data: doctors } = await res.json();
     return (
-        <Box sx={{
-            my: 10,
-            py: 30,
-            backgroundColor: "rgba(20,20,20,0.1)",
-            clipPath: "polygon(0 0, 100% 25%, 100% 100%, 0 75%)"
-        }}>
-            <Container
-                sx={{
-                    margin: "40px 0px",
-                    textAlign: "start",
-                }}
-            >
-                <Box
-                    sx={{
-                        textAlign: "center",
-                    }}
+        <Box
+            sx={{
+                my: 10,
+                py: 30,
+                backgroundColor: 'rgba(20, 20, 20, 0.1)',
+                clipPath: 'polygon(0 0, 100% 25%, 100% 100%, 0 75%)',
+            }}
+        >
+            <Box sx={{ textAlign: 'center' }}>
+                <Typography variant='h4' component='h1' fontWeight={700}>
+                    Our Top Rated Doctors
+                </Typography>
+                <Typography
+                    component='p'
+                    fontSize={18}
+                    fontWeight={400}
+                    sx={{ mt: 2 }}
                 >
-                    <Typography variant="h4" fontWeight={600}>
-                        Explore Treatment Across Specialties
-                    </Typography>
-                    <Typography component="p" fontSize={18} fontWeight={300}>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing.
-                    </Typography>
-                </Box>
-            </Container>
-            <Container sx={{
-                margin: "30px auto"
-            }}>
+                    Access to expert physicians and surgeons, advanced technologies
+                </Typography>
+                <Typography component='p' fontSize={18} fontWeight={400}>
+                    and top-quality surgery facilities right here.
+                </Typography>
+            </Box>
+
+            <Container sx={{ margin: '30px auto' }}>
                 <Grid container spacing={2}>
                     {doctors.map((doctor: any) => (
                         <Grid item key={doctor.id} md={4}>
-                            <Card >
-                                <Box>
+                            <Card>
+                                <Box
+                                    sx={{
+                                        width: '100%',
+                                        height: 300,
+                                        '& img': {
+                                            width: '100%',
+                                            height: '100%',
+                                            overflow: 'hidden',
+                                            objectFit: 'cover',
+                                        },
+                                    }}
+                                >
                                     <Image
-                                        height={100}
-                                        width={500}
-
                                         src={doctor.profilePhoto}
-                                        title="green iguana"
-                                        alt="doctor"
+                                        alt='doctor'
+                                        width={500}
+                                        height={100}
                                     />
                                 </Box>
-
                                 <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
+                                    <Typography
+                                        gutterBottom
+                                        variant='h5'
+                                        component='div'
+                                    >
                                         {doctor.name}
                                     </Typography>
-                                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                        {doctor.designation}, {doctor.gender}, {doctor.contactNumber}
+                                    <Typography variant='body2' color='text.secondary'>
+                                        {doctor.qualification}, {doctor.designation}
+                                    </Typography>
+                                    <Typography
+                                        variant='body2'
+                                        color='text.secondary'
+                                        mt={1}
+                                    >
+                                        {doctor.address}
                                     </Typography>
                                 </CardContent>
-                                <CardActions>
-                                    <Button size="small">Share</Button>
-                                    <Button size="small">Learn More</Button>
+                                <CardActions
+                                    sx={{
+                                        justifyContent: 'space-between',
+                                        px: 2,
+                                        paddingBottom: '20px',
+                                    }}
+                                >
+                                    <Button>Book Now</Button>
+                                    <Button variant='outlined'>View Profile</Button>
                                 </CardActions>
                             </Card>
                         </Grid>
                     ))}
                 </Grid>
-
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                    }}
+                >
+                    <Button
+                        variant='outlined'
+                        sx={{
+                            marginTop: '20px',
+                        }}
+                        component={Link}
+                        href='/doctors'
+                    >
+                        View ALL
+                    </Button>
+                </Box>
             </Container>
-        </Box >
+        </Box>
 
     );
 };
