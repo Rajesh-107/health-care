8dd5539230faeb187def5aec7c84c3fc615ae2d5
Rajesh-107 <applerb326@gmail.com>
2025-03-20 20:24:29 +0600

    admin super admin

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -3,36 +3,26 @@ import MailIcon from '@mui/icons-material/Mail';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import assets from "@/assets";
 import Image from "next/image";
+import { drawerItems } from "@/utils/drawerItems";
+import { UserRole } from "@/types";
 const Sidebar = () => {
     const drawer = (
         <div>
             <Toolbar />
             <Divider />
             <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
+                {drawerItems("admin" as UserRole).map((item, index) => (
+                    <ListItem key={index} disablePadding>
                         <ListItemButton>
                             <ListItemIcon>
                                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                             </ListItemIcon>
-                            <ListItemText primary={text} />
+                            <ListItemText primary={item.title} />
                         </ListItemButton>
                     </ListItem>
                 ))}
             </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
+
         </div>
     );
     return <Box>
